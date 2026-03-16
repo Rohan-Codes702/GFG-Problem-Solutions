09b54f566ce9dab92add34fdec37c562d0a4c518
Rohan <rohanmane702@gmail.com>
2026-03-16 19:22:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Tom and Jerry/tom-and-jerry.java
@@ -0,0 +1,11 @@
+// User function Template for Java
+
+class Solution {
+    static int numsGame(int N) {
+        // code here
+        if(N%2==0){
+            return 1;
+        }
+        return 0;''
+    }
+};
