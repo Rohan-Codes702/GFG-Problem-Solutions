a6ac923583f14650cc6b2082d268217adddfa80f
Rohan <rohanmane702@gmail.com>
2026-04-26 07:49:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.java
+++ Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.java
@@ -1,34 +1,26 @@
-import java.util.*;
-
 class Solution {
-    // Function to find common elements in three arrays.
-    public List<Integer> commonElements(List<Integer> arr1, List<Integer> arr2,
-                                        List<Integer> arr3) {
-        // Code Here
-        List<Integer> list = new ArrayList<>();
+    public ArrayList<Integer> commonElements(int[] a, int[] b, int[] c) {
+        // code here
+         ArrayList<Integer>list=new ArrayList<>();
+         HashSet<Integer>bset=new HashSet<>();
+          HashSet<Integer>cset=new HashSet<>();
+        HashSet<Integer>added=new HashSet<>();
+
+          
+          for(int i:b){
+              bset.add(i);
+              
+          }
+         for(int i:c){
+             cset.add(i);
+         }
          
-        Set<Integer> set1 = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>();
-          
-        for(int i = 0; i < arr1.size(); i++){
-            set1.add(arr1.get(i));
-        }
-          
-        for(int i = 0; i < arr2.size(); i++){
-            set2.add(arr2.get(i));
-        }
-        
-        for(int i = 0; i < arr3.size(); i++){
-            if(set1.contains(arr3.get(i)) && set2.contains(arr3.get(i)) 
-               && !list.contains(arr3.get(i))){
-                list.add(arr3.get(i));
-            }
-        }
-        
-        if(list.size() == 0){
-            list.add(-1);
-        }
-        
-        return list;
+         for(int i=0;i<a.length;i++){
+             if(bset.contains(a[i])&& cset.contains(a[i]) && !added.contains(a[i])){
+                 list.add(a[i]);
+                 added.add(a[i]);
+             }
+         }
+         return list;
     }
 }
