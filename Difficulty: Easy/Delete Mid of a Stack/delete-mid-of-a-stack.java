2a54591e5758d074f879c15b3f70be2bce277093
Rohan <rohanmane702@gmail.com>
2026-05-28 10:57:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete Mid of a Stack/delete-mid-of-a-stack.java
@@ -0,0 +1,9 @@
+class Solution {
+    // Function to delete middle element of a stack.
+    public void deleteMid(Stack<Integer> s) {
+        // code here
+        int size=s.size();
+        int idx=(size-1)/2;
+        s.remove(idx);
+    }
+}
