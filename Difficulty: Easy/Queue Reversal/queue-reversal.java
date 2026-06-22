f3f63560e0ee0cb59d55a9d1039d34b6472350fc
Rohan <rohanmane702@gmail.com>
2026-06-22 18:45:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Queue Reversal/queue-reversal.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseQueue(Queue<Integer> q) {
+        // code here
+        Stack<Integer>stk=new Stack<>();
+        
+        while(!q.isEmpty()){
+            stk.push(q.poll());
+        }
+        
+        while(!stk.isEmpty()){
+            q.add(stk.pop());
+        }
+    }
+}
