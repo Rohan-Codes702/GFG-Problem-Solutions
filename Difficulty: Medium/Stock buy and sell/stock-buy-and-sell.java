9ab3c2d1c9826569328843f24e08354a78e04c5a
Rohan <rohanmane702@gmail.com>
2026-06-30 15:48:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stock buy and sell/stock-buy-and-sell.java
@@ -0,0 +1,14 @@
+class Solution {
+    int stockBuySell(int arr[]) {
+        // code here
+        int maxprofit=0;
+       
+        
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>arr[i-1]){
+                maxprofit+=arr[i]-arr[i-1];
+            }
+        }
+        return maxprofit;
+    }
+}
