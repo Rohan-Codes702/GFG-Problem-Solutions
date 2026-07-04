8ad417dd8a479e5abbd4543b95ebd3e79daa2fe8
Rohan <rohanmane702@gmail.com>
2026-07-04 18:26:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Streams Sum/java-streams-sum.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static int sumUsingStreams(int[] arr) {
+        // code here
+        int sum=0;
+        
+        for(int i:arr){
+            sum+=i;
+        }
+        return sum;
+    }
+}
